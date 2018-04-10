ba0cab23b0fe0f1385c66c732d8c65537add3f18
James <smythie_89@hotmail.com>
2018-04-10 18:43:54 +0400

    Adding redux saga

--- reducers/reducer.js
+++ reducers/reducer.js
@@ -2,12 +2,16 @@ import * as types from '../constants/actionTypes';
 
 export function reducer(state = {}, action) {
 
+  // console.log(action);
+
   switch (action.type) {
     case 'API_CALL_REQUEST':
       return { ...state, fetching: true, error: null };
       break;
     case 'API_CALL_SUCCESS':
-      return { ...state, fetching: false, weather: action };
+      const bbbb = { ...state, fetching: false, weather: action };
+      console.log('this is bbbb', bbbb);
+      return bbbb;
       break;
     case 'API_CALL_FAILURE':
       return { ...state, fetching: false, dog: null, error: action.error };
--- reducers/sagas.js
+++ reducers/sagas.js
@@ -3,27 +3,43 @@ import axios from 'axios';
 
 // our worker saga
 export function* createLessonAsync() {
+  
+    try {
 
-  try {
-    // trying to call our api
-    // const ceilingLat = Math.floor(position.coords.latitude);
-    // const ceilingLng = Math.floor(position.coords.longitude);
-    // const url = `http://api.openweathermap.org/data/2.5/forecast?lat=${ceilingLat}&lon=${ceilingLng},&mode=json&appid=fb161b8bdfd1a946ed269b0b2cf42b77`;  
-    
-    const url = 'https://jsonplaceholder.typicode.com/users'
+      // navigator.geolocation.getCurrentPosition(function(position) {
+      //   const ceilingLat = Math.floor(position.coords.latitude);
+      //   const ceilingLng = Math.floor(position.coords.longitude);
+      // });
 
-    // axios will take 2 or 3 arguments. the method (axios.get/axios.post, url, arguments);
-    const response = yield call(axios.get, url);
-    console.log(response);
-  } catch(error) {
-    // throwing an error from our api
-    console.log('request failed', error);
-  }
+      // trying to call our api
+
+      let lat = 37;
+      let lng = -123;
+      
+      const url = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lng},&mode=json&appid=fb161b8bdfd1a946ed269b0b2cf42b77`;  
+      
+      // const url = 'https://jsonplaceholder.typicode.com/users'
+  
+      // axios will take 2 or 3 arguments. the method (axios.get/axios.post, url, arguments);
+      const response = yield call(axios.get, url);
+      //console.log(response.data);
+  
+      yield put({ 'type': 'API_CALL_SUCCESS', response: response.data });
+    } catch(error) {
+      // throwing an error from our api
+      //console.log('request failed', error);
+      yield put({ 'type': 'API_CALL_ERROR', response: 'error bitch' });
+    }
+  // });
+
+
 }
 
 // our watcher saga
 export function* watchCreateLesson() {
-  console.log('redux saga is running the createLesson action listener');
+  //console.log('redux saga is running the createLesson action listener');
+
+  
   yield takeEvery('API_CALL_REQUEST', createLessonAsync);
 }
 
