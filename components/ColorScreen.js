24c077c0751c807529b4d5d822f6c73b6744b3ae
James <smythie_89@hotmail.com>
2018-06-09 17:33:53 +0400

    Added colorscreen

---
+++ components/ColorScreen.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { View, Text, Button } from 'react-native';
+import WeatherSvg from './WeatherSvg';
+import styles from '../assets/styles/styles';
+
+export default class ColorScreen extends Component {
+  render() {
+
+    const { navigation } = this.props;
+    const temperature = navigation.getParam('temperature');
+    const weatherType = navigation.getParam('weatherType');
+    const sectionTemperature = styles[`section${temperature}`];
+
+    return (
+      <View style={[styles.section, sectionTemperature]}>
+        <View style={styles.sectionInner}>
+          <Text style={styles.sectionText}>
+            {temperature}&#176;c and {weatherType.toLowerCase()} right now
+          </Text>
+          <WeatherSvg weatherType={weatherType} />
+        </View>
+        <Text style={styles.sectionText}>
+          Does this color match how it feels outside?
+        </Text>
+        <Button title="Yes" />
+        <Button 
+          title="No" 
+          onPress={() => this.props.navigation.navigate('MultipleColorScreen')} />
+      </View>
+    )
+  }
+}
