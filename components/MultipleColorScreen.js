40363a899899d19034e847c24143dcea2dfc273e
James <smythie_89@hotmail.com>
2018-06-30 17:02:50 +0400

    Going to revamp the color selector

--- components/MultipleColorScreen.js
+++ components/MultipleColorScreen.js
@@ -7,13 +7,23 @@ export default class MultipleColorsScreen extends Component {
     super(props);
 
     this.state = {
-      pan: new Animated.ValueXY(),
+      pan: {
+        one: new Animated.ValueXY(),
+        two: new Animated.ValueXY(),
+        three: new Animated.ValueXY()
+      },
       scale: new Animated.Value(1),
       selectedColor: this.props.navigation.getParam('selectedColor'),
+      selectedColor: [100, 150, 200],
       width: Dimensions.get('window').width,
       height: Dimensions.get('window').height,
-      circlePosX: 0,
-      circlePosY: 0
+      circle1PosX: 0,
+      circle1PosY: 0,
+      circle2PosX: 0,
+      circle2PosY: 0,
+      circle3PosX: 0,
+      circle3PosY: 0,
+      pans: [this._panResponder, this._panResponder1, this._panResponder2]
     }
 
     this.updateBackgroundColor = this.updateBackgroundColor.bind(this);
@@ -21,47 +31,45 @@ export default class MultipleColorsScreen extends Component {
 
   // when the component mounts we can position the correct location of the square
   componentDidMount() {
-    this.positionSquare();
+    // this.positionSquare();
   }
 
   componentWillMount() {
-    this.initPanResponder();
-  }
+    for(let i = 0; i < this.state.pans.length; i++) {
 
-  initPanResponder() {
-    this._panResponder = PanResponder.create({
-      onMoveShouldSetResponderCapture: () => true,
-      onMoveShouldSetPanResponderCapture: () => true,
+      const selector = this.state.pan[Object.keys(this.state.pan)[i]];
 
-      onPanResponderGrant: (e, gestureState) => {
-        // Set the initial value to the current state
-        this.state.pan.setOffset({x: this.state.pan.x._value, y: this.state.pan.y._value});
-        this.state.pan.setValue({x: 0, y: 0});
-      },
+      this.state.pans[i] = PanResponder.create({
+        onMoveShouldSetResponderCapture: () => true,
+        onMoveShouldSetPanResponderCapture: () => true,
 
-      onPanResponderRelease: (e, gestureState) => {
-        // console.log('innnn', gestureState.moveX, gestureState.moveY);
-        // this.state.pan.setValue({x: gestureState.moveX, y: gestureState.moveY});
-      },
+        // Initially, set the value of x and y to 0 (the center of the screen)
+        onPanResponderGrant: (e, gestureState) => {
+          selector.setOffset({x: selector.x._value, y: selector.y._value});
+          selector.setValue({x: 0, y: 0});
+        },
 
-      onPanResponderMove: (e, gestureState) => {
-        console.log('grant', gestureState.moveX, gestureState.moveY);
-        console.log('dimensionssss', this.state.width * this.state.height);
+        onPanResponderMove: (e, gestureState) => {
+          console.log(gestureState.moveX, gestureState.moveY);  
+          this.setState({
+            circle1PosX: gestureState.moveX,
+            circle1PosY: gestureState.moveY
+          });
+  
+          Animated.event([null, {
+            dx: selector.x,
+            dy: selector.y
+          }])(e, gestureState);
+  
+          // now we can call the background color and change it.
+          this.updateBackgroundColor();
+        },
 
-        this.setState({
-          circlePosX: gestureState.moveX,
-          circlePosY: gestureState.moveY
-        });
-
-        Animated.event([null, {
-          dx: this.state.pan.x,
-          dy: this.state.pan.y,
-        }])(e, gestureState); // <<--- INVOKING HERE!
-
-        // now we can call the background color and change it.
-        this.updateBackgroundColor();
-      }
-    });
+        onPanResponderRelease: (e, {vx, vy}) => {
+          selector.flattenOffset();
+        }
+      });
+    }
   }
 
   positionSquare() {
@@ -86,28 +94,35 @@ export default class MultipleColorsScreen extends Component {
 
     // this now has positioned the square on the screen.
     // we now will only calculate the diagonal when we need to change thw background color
-    this.state.pan.setOffset({x: squarePosW, y: squarePosH});
+    this.state.pan.one.setOffset({x: 200, y: squarePosH});
   }
 
   // getting the correct RGB values using the position of the square
   updateBackgroundColor() {
 
-    // get position of the circle.
-    const positionX = this.state.circlePosX;
-    const positionY = this.state.circlePosY;
-    const positionD = Math.sqrt((positionX * positionX) + (positionY * positionY)); // diagonal position of the screen rectangle
+    // // get position of the circle.
+    // const positionX = this.state.circle1PosX;
+    // const positionY = this.state.circle1PosY;
+    // const positionD = Math.sqrt((positionX * positionX) + (positionY * positionY)); // diagonal position of the screen rectangle
 
-    // 255 is the RBG color limits. So we divide 255 by the width, height and diagonal to know what to multiply the position by
-    const fractionX = 255 / this.state.width;
-    const fractionY = 255 / this.state.height;
-    const fractionD = 255 / Math.sqrt((this.state.width * this.state.width) + (this.state.height * this.state.height));
+    // // 255 is the RBG color limits. So we divide 255 by the width, height and diagonal to know what to multiply the position by
+    // const fractionX = 255 / this.state.width;
+    // const fractionY = 255 / this.state.height;
+    // const fractionD = 255 / Math.sqrt((this.state.width * this.state.width) + (this.state.height * this.state.height));
 
-    // finally we round multiply the fractions by the position to get a 255 limit value
-    const r = Math.round(fractionX * positionX);
-    const g = Math.round(fractionY * positionY);
-    const b = Math.round(fractionD * positionD);
+    // // finally we round multiply the fractions by the position to get a 255 limit value
+    // const r = Math.round(fractionX * positionX);
+    // const g = Math.round(fractionY * positionY);
+    // const b = Math.round(fractionD * positionD);
+
+    const positionX = Math.round(this.state.circle1PosX);
+    const positionY = Math.round(this.state.circle1PosY);
+
+    const y = Math.round((255 / this.state.height) * this.state.circle1PosY);
+
+    console.log(positionX, y);
     
-    const arr = [r, g, b];
+    const arr = [100, 100, y];
     this.setState({
       selectedColor: arr
     });
@@ -118,18 +133,34 @@ export default class MultipleColorsScreen extends Component {
     let { pan, scale } = this.state;
 
     // Calculate the x and y transform from the pan value
-    let [translateX, translateY] = [pan.x, pan.y];
+    let [translateX, translateY] = [pan[Object.keys(pan)[0]].x, pan[Object.keys(pan)[0]].y];
     let rotate = '0deg';
 
     // Calculate the transform property and set it as a value for our style which we add below to the Animated.View component
     let imageStyle = {transform: [{translateX}, {translateY}, {rotate}, {scale}]};
 
     return (
-      <View
-        style={[styles.section, styles.sectionThird, { 'backgroundColor': `rgb(${this.state.selectedColor[0]}, ${this.state.selectedColor[1]}, ${this.state.selectedColor[2]})`}]}>
-        <Animated.View style={[styles.colorPicker, imageStyle]} {...this._panResponder.panHandlers}>
-        <Text>{this.state.selectedColor[0]} {this.state.selectedColor[1]} {this.state.selectedColor[2]}</Text>
-        </Animated.View>
+      <View style={styles.container}>
+        <View
+          style={[styles.section, styles.sectionThird, { 'backgroundColor': `rgb(${this.state.selectedColor[0]}, ${this.state.selectedColor[1]}, ${this.state.selectedColor[2]})`}]}>
+          <Animated.View style={[styles.colorPicker, imageStyle]} {...this.state.pans[0].panHandlers}>
+            <Text>
+              0 - {this.state.selectedColor[0]} {this.state.selectedColor[1]} {this.state.selectedColor[2]}
+            </Text>
+          </Animated.View>
+
+          <Animated.View style={[styles.colorPicker, imageStyle]} {...this.state.pans[1].panHandlers}>
+            <Text>
+              1 - {this.state.selectedColor[0]} {this.state.selectedColor[1]} {this.state.selectedColor[2]}
+            </Text>
+          </Animated.View>
+
+          <Animated.View style={[styles.colorPicker, imageStyle]} {...this.state.pans[2].panHandlers}>
+            <Text>
+              2 - {this.state.selectedColor[0]} {this.state.selectedColor[1]} {this.state.selectedColor[2]}
+            </Text>
+          </Animated.View>
+        </View>
       </View>
     );
   }
